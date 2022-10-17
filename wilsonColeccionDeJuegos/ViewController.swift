bad7ec7e506d051be941373f0377e77d9689e81e
wilsonTec <wilson.garcia@tecsup.edu.pe>
2022-10-17 14:15:28 -0500

    Initial Commit

---
+++ wilsonColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wilsonColeccionDeJuegos
+//
+//  Created by Mac 18 on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
